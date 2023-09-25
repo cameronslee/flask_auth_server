6cb64c8ea2706575fb4c4d690d2f6facd9d6d48d
Cameron Lee <cameronslee@hotmail.com>
2023-09-25 10:30:50 -0700

    client login

--- client/client_server.py
+++ client/client_server.py
@@ -1,12 +1,28 @@
 import json
-
-from flask import Flask, request
+import requests
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
+AUTH_PATH = 'http://localhost:8001/auth'
+TOKEN_PATH = 'http://localhost:8001/token'
+RES_PATH = 'http://localhost:8002/users'
+REDIRECT_URL = 'http://localhost:8000/callback'
+
+CLIENT_ID = 'sample-client-id'
+CLIENT_SECRET = 'sample-client-secret'
+FLOW = 'Authorization Code'
 
 @app.before_request
 def before_request():
   pass
+
+@app.route('/login')
+def login():
+  return render_template('login.html',
+                         dest = AUTH_PATH,
+                         client_id = CLIENT_ID,
+                         response_type = FLOW,
+                         redirect_url = REDIRECT_URL)
 
 if __name__ == '__main__':
   app.run(port = 8000, debug = True)
