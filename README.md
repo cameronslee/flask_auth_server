a47e3c51d67e3a8b8ac207a0843e69a77fb4e38f
Cameron Lee <cameronslee@hotmail.com>
2023-09-25 10:16:40 -0700

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask OAuth2.0 Server
+
+Client Server: port 8000
+Auth Server: port 8001
+API Server: port 8002
---
+++ api/api_server.py
@@ -0,0 +1,10 @@
+import json
+import ssl
+
+from flask import Flask, request
+
+def before_request():
+  pass
+
+if __name__ == '__main__':
+  app.run(port = 8002, debug = True)
