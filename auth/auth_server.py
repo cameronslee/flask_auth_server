3152efdfa511e3f06835e76a9605f8653276e575
Cameron Lee <cameronslee@hotmail.com>
2023-09-27 19:04:59 -0700

    token handling, error handling

--- auth/auth_server.py
+++ auth/auth_server.py
@@ -1,5 +1,6 @@
 ### AUTH SERVER ###
 import json
+import jwt
 import time
 
 import sqlite3
@@ -10,9 +11,13 @@ import urllib.parse as urlparse
 from urllib.parse import urlencode
 from cryptography.fernet import Fernet
 
-from flask import Flask, request, redirect, render_template, g, current_app, flash
+from flask import Flask, request, Response, redirect, render_template, g, flash
+
+with open('private_key.pem', 'rb') as file:
+  private_key = file.read()
 
 app = Flask(__name__)
+
 app.config.from_mapping(
   SECRET_KEY='DEV',
   )
@@ -32,7 +37,6 @@ def close_db(e=None):
     if db is not None:
       db.close()
 
-# Setup DB
 def init_db():
   db = get_db()
   with app.open_resource('schema.sql') as f:
@@ -47,23 +51,51 @@ def init_db_command():
 app.teardown_appcontext(close_db)
 app.cli.add_command(init_db_command)
 
-##################################################
 KEY = Fernet.generate_key()
+ISSUER = 'foo-auth-server'
 MAX_AUTH_CODE_LIFETIME = 600 # 10min (RFC6749 pg26)
-##################################################
+JWT_LIFETIME = 1800
 
-# TODO
 def authenticate_client(client_id, client_secret):
-  # Query DB, check that client_id and client_secret exists
-  return True
+  db = get_db()
+  record = db.execute('SELECT * FROM applications WHERE client_secret= ? AND client_id= ?', (client_secret,client_id)).fetchone()
+  return False if not record else True
 
-# TODO: add verifcation for valid client, would be done upon registering an application
 def verify_client(client_id, redirect_uri):
-  return True
+  db = get_db()
+  record = db.execute('SELECT * FROM clients WHERE client_id= ? AND redirect_uri= ?', (client_id,redirect_uri)).fetchone()
+  return False if not record else True
 
-# TODO
 def authenticate_user(username, password):
-  # Query DB, check that username and password exists
+  db = get_db()
+  record = db.execute('SELECT * FROM users WHERE username= ? AND password= ?', (username,password)).fetchone()
+  return False if not record else True
+
+def generate_access_token():
+  payload = {
+    "iss": ISSUER,
+    "exp": time.time() + JWT_LIFETIME
+  }
+
+  access_token = jwt.encode(payload, private_key, algorithm = 'RS256')
+
+  return access_token
+
+def verify_authorization_code(authorization_code, client_id, redirect_uri):
+  db = get_db()
+  authorization_code = authorization_code.encode()
+  record = db.execute('SELECT * FROM auth_codes WHERE authorization_code= ?', (authorization_code,)).fetchone()
+
+  if not record:
+    return False
+
+  record_exp = record['exp']
+  record_client_id = record['client_id']
+  record_redirect_uri = record['redirect_uri']
+
+  if time.time() > record_exp or record_client_id != client_id or record_redirect_uri != redirect_uri:
+    return False
+
   return True
 
 def generate_authorization_code(client_id, redirect_uri):
@@ -79,8 +111,8 @@ def generate_authorization_code(client_id, redirect_uri):
   expiration_date = time.time() + MAX_AUTH_CODE_LIFETIME
  
   try:
-    db.execute("INSERT INTO auth_codes(exp, client_id, redirect_uri) VALUES (?,?,?)",
-    (expiration_date, client_id, redirect_uri)
+    db.execute("INSERT INTO auth_codes(authorization_code, exp, client_id, redirect_uri) VALUES (?,?,?,?)",
+    (authorization_code, expiration_date, client_id, redirect_uri)
     )
     db.commit()
   except db.IntegrityError:
@@ -108,11 +140,10 @@ def auth():
                          client_id = client_id,
                          redirect_uri = redirect_uri)
 
-def process_redirect_uri(redirect_url, authorization_code):
-# Prepare the redirect URL
+def form_redirect_uri(redirect_url, authorization_code):
   url_parts = list(urlparse.urlparse(redirect_url))
   queries = dict(urlparse.parse_qsl(url_parts[4]))
-  queries.update({ "authorization_code": authorization_code })
+  queries.update({ "code": authorization_code })
   url_parts[4] = urlencode(queries)
   url = urlparse.urlunparse(url_parts)
   return url
@@ -124,23 +155,71 @@ def signin():
   password = request.form.get('password')
   client_id = request.form.get('client_id')
   redirect_uri = request.form.get('redirect_uri')
-
+  
+  # Error Response
   if None in [ username, password, client_id, redirect_uri ]:
-    print(username, password, client_id, redirect_uri)
     return json.dumps({
       "error": "invalid_request"
     }), 400
+
+  if not verify_client(client_id, redirect_uri):
+    return json.dumps({
+      'error': 'unauthorized_client'
+    }), 401
 
   if not authenticate_user(username, password):
     return json.dumps({
       'error': 'access_denied'
     }), 401
 
+  response = Response()
+  if response.status_code != 200:
+    error_text = json.loads(request.text).get('error')
+    print(error_text)
+    if error_text == 'unsupported_response_type' or error_text == 'invalid_scope' or error_text == 'server_error':
+      return json.dumps({
+        'error': error_text 
+      }), 500
+    elif error_text == 'temporarily_unavailable':
+      return json.dumps({
+        'error': error_text
+      }), 503
+
   authorization_code = generate_authorization_code(client_id, redirect_uri)
 
-  uri = process_redirect_uri(redirect_uri, authorization_code)
+  uri = form_redirect_uri(redirect_uri, authorization_code)
+  
+  return redirect(uri, code = 302)
 
-  return redirect(uri, code = 303)
+@app.route('/token', methods = ['POST'])
+def exchange_for_token():
+  authorization_code = request.form.get('authorization_code')
+  client_id = request.form.get('client_id')
+  client_secret = request.form.get('client_secret')
+  redirect_url = request.form.get('redirect_url')
+
+  if None in [ authorization_code, client_id, client_secret, redirect_url ]:
+    return json.dumps({
+      "error": "invalid_request"
+    }), 400
+
+  if not authenticate_client(client_id, client_secret):
+    return json.dumps({
+      "error": "invalid_client"
+    }), 400
+
+  if not verify_authorization_code(authorization_code, client_id, redirect_url):
+    return json.dumps({
+      "error": "access_denied"
+    }), 400
+
+  access_token = generate_access_token()
+  
+  return json.dumps({ 
+    "access_token": access_token.decode(),
+    "token_type": "JWT",
+    "expires_in": JWT_LIFETIME
+  })
 
 if __name__ == '__main__':
   app.run(port = 8001, debug = True)
